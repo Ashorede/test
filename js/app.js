ba812580e0626291c34d7352118c1ac487db39c2
Ashorede <2580750157@qq.com>
2025-01-21 01:14:09 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,44 +1,52 @@
 function reset() {
-
+			
 	document.getElementById('Long-axis_diamater').value = '';
 	document.getElementById('Texture').value = '';
 	document.getElementById('Bubblelike_lucency').value = '';
-	document.getElementById('Air_bronchogram').value = '';
-	document.getElementById('Vascular_convergence').value = '';
-	document.getElementById('Pleural_retraction').value = '';
+	document.getElementById('Air bronchogram').value = '';
+	document.getElementById('Vascular convergence').value = '';
+	document.getElementById('Pleural retraction').value = '';
 	document.getElementById('SEX').value = '';
-	document.getElementById('Smoke_history').value = '';
+	document.getElementById('Smoke history').value = '';
 }
-		
+					
 function predict() {
-
-    let input = [];
-    input.push(parseFloat(document.getElementById('Long-axis_diamater').value));
-    input.push(parseFloat(document.getElementById('Texture').value));
-    input.push(parseFloat(document.getElementById('Bubblelike_lucency').value));
-    input.push(parseFloat(document.getElementById('Air bronchogram').value));
-    input.push(parseFloat(document.getElementById('Vascular convergence').value));
-    input.push(parseFloat(document.getElementById('Pleural retraction').value));
-    input.push(parseFloat(document.getElementById('SEX').value));
-    input.push(parseFloat(document.getElementById('Smoke history').value));
-
-    if (input.length === 8) {
-        fetch('http://192.168.43.80:5000/predict', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ data: input })
-        })
-        .then(response => response.json())
-        .then(data => {
-
-            document.getElementById('prediction').textContent = data.prediction;
-        })
-        .catch(error => {
-            console.error('error:', error);
-        });
-    } else {
-        alert("num error！");
-    }
+			
+	let input = [];
+	var Long = document.getElementById('Long-axis_diamater').value
+	var texture = document.getElementById('Texture').value
+	var Bubblelike_lucency = document.getElementById('Bubblelike_lucency').value
+	var air = document.getElementById('Air bronchogram').value
+	var Vascular = document.getElementById('Vascular convergence').value
+	var Pleural = document.getElementById('Pleural retraction').value
+	var sex = document.getElementById('SEX').value
+	var smoke = document.getElementById('Smoke history').value
+				
+	if (Long.trim() != "" && texture.trim() != "" && Bubblelike_lucency.trim() != "" && air.trim() != "" && Vascular.trim() != "" && Pleural.trim() != "" && sex.trim() != "" && smoke.trim() != "") {
+		input.push(parseFloat(Long))
+		input.push(parseFloat(texture))
+		input.push(parseFloat(Bubblelike_lucency))
+		input.push(parseFloat(air))
+		input.push(parseFloat(Vascular))
+		input.push(parseFloat(Pleural))
+		input.push(parseFloat(sex))
+		input.push(parseFloat(smoke))
+		fetch('http://127.0.0.1:5000/predict', {
+			method: 'POST',
+			headers: {
+			    'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ data: input })
+		})
+		.then(response => response.json())
+		.then(data => {
+			
+			document.getElementById('prediction').textContent = data.prediction;
+		})
+		.catch(error => {
+			console.error('error:', error);
+		});
+	} else {
+		alert("num error");
+	}
 }
